c589d5c1e018749d6a74155cf593592a07340ff1
Mesayaaa <127949657+Mesayaaa@users.noreply.github.com>
2023-04-01 12:16:22 +0700

    coba game

--- MainWindow/character_assets/animasi.js
+++ MainWindow/character_assets/animasi.js
@@ -1,36 +1,53 @@
-$(document).ready(function() {
+$(document).ready(function () {
   const image = $("#gambar");
-  const originalImg = image.attr('src');
+  const originalImg = image.attr("src");
   let intervalId = null;
 
-  $("#makan").on("click", function() {
+  $("#makan").on("click", function () {
     clearInterval(intervalId);
-    image.attr('src', "character_assets/Scene - Yellow Character/(Scene) Eat Burger/Versi Gif.gif");
-    image.css('transform', `scale(1.4)`);
-    intervalId = setInterval(function(){
-      image.css('transform', `scale(1)`);
-      image.attr('src', originalImg);
+    image.attr(
+      "src",
+      "character_assets/Scene - Yellow Character/(Scene) Eat Burger/Versi Gif.gif"
+    );
+    image.css({
+      transform: "scale(1.3)",
+      "margin-top": "-5px",
+    });
+    intervalId = setInterval(function () {
+      image.css({
+        transform: "scale(1)",
+        "margin-top": "-20px",
+      });
+      image.attr("src", originalImg);
       clearInterval(intervalId);
     }, 1200);
   });
 
-  $("#tidur").on("click", function() {
-    image.attr("src", "../Select_Character/bg_assetChar/Scene - Yellow Character/(Scene) Sleep/Versi Gif.gif");
-    
+  $("#tidur").on("click", function () {
+    image.attr(
+      "src",
+      "../Select_Character/bg_assetChar/Scene - Yellow Character/(Scene) Sleep/Versi Gif.gif"
+    );
   });
 
-  $("#obat").on("click", function() {
+  $("#obat").on("click", function () {
     clearInterval(intervalId);
-    image.attr('src', "character_assets/Scene - Yellow Character/(Scene) Minum Obat/Versi gif.gif");
-    image.css('transform', `scale(1.5)`);
-    intervalId = setInterval(function(){
-      image.css('transform', `scale(1)`);
-      image.attr('src', originalImg);
+    image.attr(
+      "src",
+      "character_assets/Scene - Yellow Character/(Scene) Minum Obat/Versi gif.gif"
+    );
+    image.css("transform", `scale(1.5)`);
+    intervalId = setInterval(function () {
+      image.css("transform", `scale(1)`);
+      image.attr("src", originalImg);
       clearInterval(intervalId);
     }, 3000);
   });
 
-  $("#main").on("click", function() {
-    image.attr("src", "../Select_Character/bg_assetChar/Scene - Yellow Character/(Scene) Play (2)/Versi Gif.gif");
+  $("#main").on("click", function () {
+    image.attr(
+      "src",
+      "../Select_Character/bg_assetChar/Scene - Yellow Character/(Scene) Play (2)/Versi Gif.gif"
+    );
   });
 });
